3015edfac4ce00810eeae125fe548b896592209f
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2023-07-28 19:55:50 +0530

    Add navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,63 @@
+import { Link, Outlet } from 'react-router-dom';
+
 const NavBar = () => {
-  return
-}
+  const showNavbar = () => {
+    document.querySelector('header .navbar').classList.toggle('hidden');
+  };
+
+  return (
+    <>
+      <header className="p-4 bg-black flex justify-between font-mono font-bold">
+        <h1 className="text-3xl text-white">Fake Store</h1>
+        <div className="mobile text-white md:hidden">
+          <Link
+            className="text-2xl relative top-0.4 hover:scale-110 inline-block"
+            to={'cart'}
+          >
+            <i className="fa-solid fa-cart-shopping"></i>
+          </Link>
+          <button
+            onClick={showNavbar}
+            className="text-2xl ml-9 transition-all hover:scale-110"
+          >
+            <i className="fa-solid fa-list"></i>
+          </button>
+        </div>
+        <div className="navbar hidden md:block fixed top-20 right-1 md:static text-white bg-black p-3 ">
+          <nav className="text-base flex flex-col gap-5 md:block">
+            <Link
+              onClick={showNavbar}
+              className="align-middle hover:scale-110 inline-block transition-all"
+              to={'home'}
+            >
+              Home
+            </Link>
+            <Link
+              onClick={showNavbar}
+              className="align-middle md:ml-5 hover:scale-110 inline-block transition-all"
+              to={'shop'}
+            >
+              Shop
+            </Link>
+            <Link
+              onClick={showNavbar}
+              className="align-middle md:ml-5 hover:scale-110 inline-block transition-all"
+              to={'about'}
+            >
+              About
+            </Link>
+            <Link
+              className="relative top-1 hidden md:inline-block md:ml-5 hover:scale-110 transition-all"
+              to={'cart'}
+            >
+              <i className="fa-solid fa-cart-shopping"></i>
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
+};
 
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Shop from './Pages/Shop'
+import Cart from './components/Cart'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NavBar />
+    element: <NavBar />,
+    children: [
+      {path: '/home', element: <Home />},
+      {path: '/about', element: <About />},
+      {path: '/shop', element: <Shop />},
+      {path: '/cart', element: <Cart />},
+    ]
   },
 ])
 
