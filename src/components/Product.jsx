f875e5fed7004fb11505dc944da1f9af20c24556
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2023-10-02 15:10:48 +0530

    fix: product catergory search not working

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,22 +12,21 @@ const Product = () => {
           (product) => product.category != 'electronics'
         );
       } else {
-        arr = product.data.filter(
-          (product) => product.category === product.category
-        );
+        arr = product.data.filter((p) => p.category === product.category);
+        console.log(arr);
       }
       return arr.map((product, index) => {
         return (
           <div
             key={index}
-            className="flex flex-col items-center bg-white p-4 shadow-lg"
+            className='flex flex-col items-center bg-white p-4 shadow-lg'
           >
-            <div className="h-32 md:h-64 flex items-center">
-              <img src={product.image} alt=" " className="w-20 md:w-36" />
+            <div className='h-32 md:h-64 flex items-center'>
+              <img src={product.image} alt=' ' className='w-20 md:w-36' />
             </div>
-            <div className="text-sm text-center md:text-lg flex-grow">
+            <div className='text-sm text-center md:text-lg flex-grow'>
               <p>{product.title}</p>
-              <p className="font-bold mt-3">$ {product.price}</p>
+              <p className='font-bold mt-3'>$ {product.price}</p>
             </div>
             <Cart productID={product.id} />
           </div>
@@ -35,8 +34,7 @@ const Product = () => {
       });
     }
   };
-
-  return <div className="product mt-7">{mapData()}</div>;
+  return <div className='product mt-7'>{mapData()}</div>;
 };
 
 export default Product;
